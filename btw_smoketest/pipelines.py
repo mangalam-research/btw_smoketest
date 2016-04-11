664e81d72b8c2752c2533779c500643e547373bb
Louis-Dominique Dubeau <ldd@lddubeau.com>
2016-04-11 13:07:33 -0400

    Support for header checks.
    Added header checks to the spider. This introduces the ``naked``
    parameter which can be used to indicate that a site is not using a front
    end web server and thus won't have the necessary headers (and
    consequently header checks should not be performed).
    
    The format of the reports provided has also been revamped. And there is
    now a ``REPORT`` file created that contains the body of the email that
    is (or would be) sent.

--- btw_smoketest/pipelines.py
+++ btw_smoketest/pipelines.py
@@ -34,27 +34,45 @@ class JSONWriterPipeline(object):
 
             errors = [
                 i for i in items if i['status'] != 200 or
-                i['validation_error']]
+                i['validation_error'] or i['header_errors']]
             if errors:
                 with open(os.path.join(outdir, 'ERRORS'), 'w') as f:
                     f.write(json.dumps([dict(i) for i in errors]))
 
-                message = ["URL: %s\nFailed retrieval with: %d\n" %
-                           (i['url'], i['status']) for i in items if
-                           i['status'] != 200]
+                message = []
+                for i in errors:
+                    item_message = ["===\nURL: {0}\n\n".format(i['url'])]
 
-                message.append("\n")
+                    status = i['status']
+                    if status != 200:
+                        item_message.append(
+                            "Failed retrieval with status: {0}\n".format(
+                                status))
 
-                message += ["URL: %s\nFailed validation.\n" %
-                            i['url'] for i in items if
-                            i['validation_error']]
+                    if i['validation_error']:
+                        item_message.append("Failed validation.\n\n")
 
-                message.append("\nSee %s for details." % outdir)
+
+                    header_errors = i['header_errors']
+                    if header_errors:
+                        item_message.append(
+                            ("Failed header checks with the following "
+                             "errors:\n{0}\n").format(
+                                 "\n".join(header_errors)))
+
+                    if len(item_message) > 1:
+                        message += item_message
+
+                message.append("\nSee %s for details of validation errors." %
+                               outdir)
+
+                email_body = "".join(message)
+                with open(os.path.join(outdir, 'REPORT'), 'w') as f:
+                    f.write(email_body)
 
                 sender = MailSender(mailfrom="btw@btw.mangalamresearch.org")
                 sender.send(["btw@btw.mangalamresearch.org"],
-                            "Smoketest failure",
-                            "".join(message))
+                            "Smoketest failure", email_body)
             else:
                 with open(os.path.join(outdir, 'CLEAN'), 'w') as f:
                     f.write("yes\n")
--- btw_smoketest/spiders/btw.py
+++ btw_smoketest/spiders/btw.py
@@ -18,17 +18,49 @@ class LinkItem(Item):
     html_path = Field()
     validation_report_path = Field()
     validation_error = Field()
+    header_errors = Field()
 
 SHORTCUTS = {
     "demo": "https://btw-demo.mangalamresearch.org",
     "local": "http://localhost:8000",
 }
 
+def format_header_error(key, value):
+    return "{0} has no value".format(key) if value is None else \
+        "{0} has the value {1}".format(key, value)
+
+def check_header_value(header_errors, headers, key, expected_value):
+    value = headers.get(key)
+
+    # If expected_value is a callable, we call it to test the value,
+    # otherwise we just compare.
+    if not (expected_value(value) if callable(expected_value) else \
+       value == expected_value):
+        header_errors.append(
+            format_header_error(key, value))
+
+
 class BtwSpider(CrawlSpider):
     name = "btw"
 
     def __init__(self, url="https://btw.mangalamresearch.org",
-                 btw_dev=None, *args, **kwargs):
+                 btw_dev=None, naked=False, *args, **kwargs):
+        """
+        :param url: The URL of the site to test.
+
+        :param btw_dev: A value to set the ``btw_dev`` cookie to. This
+        is useful if the site is in maintenance mode. Setting
+        ``btw_dev`` to a secret value will bypass the maintenance
+        block. Defaults to ``None``.
+
+        :param naked: Whether or not the site is being accessed
+        "naked", as a server started by Django with ``runserver`` or a
+        similar command, rather than through Nginx, Apache, or some
+        other server software. Defaults to ``False``. On a naked site,
+        there are some header checks we cannot perform because they
+        are the responsibility of the server software that first
+        receives the requests.
+        """
 
         if url[0] == "@":
             # Shortcut, resolve it to a real URL
@@ -44,6 +76,8 @@ class BtwSpider(CrawlSpider):
         self.btw_dev = btw_dev \
             if btw_dev is not None and parsed.scheme == "https" \
             else None
+
+        self.naked = naked
 
         # These rules make it so that we will test all links on our site,
         # including links to external sites. However, we do not *crawl*
@@ -76,16 +110,35 @@ class BtwSpider(CrawlSpider):
         return self.handle_response(response)
 
     def handle_response(self, response):
+        headers = response.headers
         item = LinkItem()
-        item['url'] = response.url
+        url = response.url.decode("utf-8")
+        item['url'] = url
         item['referer'] = response.request.headers.get('Referer')
         item['status'] = response.status
 
-        parsed = urlparse(response.url)
+        parsed = urlparse(url)
 
         item['validation_error'] = False
+        item['header_errors'] = header_errors = []
         if parsed.hostname == self.my_domain:
-            html_path = os.path.join(self.outdir, slugify(response.url))
+            # We perform header checks only on non-naked sites.
+            if not self.naked:
+                check_header_value(header_errors, headers,
+                                   'X-Frame-Options', 'SAMEORIGIN')
+                check_header_value(header_errors, headers,
+                                   'X-Content-Type-Options', 'nosniff')
+                check_header_value(header_errors, headers,
+                                   'X-XSS-Protection', '1; mode=block')
+
+                def sts_check(value):
+                    return value is not None and value.starts_with("max-age=")
+
+                check_header_value(header_errors, headers,
+                                   'Strict-Transport-Security', sts_check)
+
+
+            html_path = os.path.join(self.outdir, slugify(url))
             validation_report_path = html_path + ".report"
             item['html_path'] = html_path
             item['validation_report_path'] = validation_report_path
